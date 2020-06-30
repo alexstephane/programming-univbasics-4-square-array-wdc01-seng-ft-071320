88a67acf67169d55fd669072b3beebfec48dc9b3
Alex Ngundji <alexngundji@gmail.com>
2020-06-30 16:39:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,14 +7,14 @@
 #   return new_numbers
 # end
 
-# def square_array(array)
-#   squared = []
-# count = 0
-#   while count < array.size do
-#     squared << (array[count] ** 2)
-#     count = count + 1
-#   end
-#   squared
-# end
+def square_array(array)
+  squared = []
+count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
+end
 
 
